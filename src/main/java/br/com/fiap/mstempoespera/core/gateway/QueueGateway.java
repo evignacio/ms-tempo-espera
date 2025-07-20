2231cfbfbf8d8fbfd474fc489be9af77b955e1fd
evignacio <reignomo27@hotmail.com>
2025-07-20 18:33:58 -0300

    .

--- src/main/java/br/com/fiap/mstempoespera/core/gateway/QueueGateway.java
+++ src/main/java/br/com/fiap/mstempoespera/core/gateway/QueueGateway.java
@@ -1,8 +1,8 @@
 package br.com.fiap.mstempoespera.core.gateway;
 
 import br.com.fiap.mstempoespera.core.entity.QueuesDetails;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 
 public interface QueueGateway {
-    Mono<QueuesDetails> findAll(String upasId);
+    Flux<QueuesDetails> findAll(String upasId);
 }
